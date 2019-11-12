1cd6cba2ca5813281e40f677ed3d8e15413b121e
张斌 <q243132465@163.com>
2019-11-12 16:23:24 +0800

    test 分支

--- shardingsphere-yml/src/main/java/com/gupaovip/shardingsphere/shardingsphereyml/ShardingsphereYmlApplication.java
+++ shardingsphere-yml/src/main/java/com/gupaovip/shardingsphere/shardingsphereyml/ShardingsphereYmlApplication.java
@@ -8,6 +8,7 @@ public class ShardingsphereYmlApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ShardingsphereYmlApplication.class, args);
+        System.out.println("yml-分支");
     }
 
 }
